eadd1fe6bc9809e0f587d6b0676eb495d39c2fe9
Tjuraid <turid.folk@live.no>
2016-10-01 21:09:46 +0200

    Lagt til oppgavetekst i oblig 3

--- oblig3.php
+++ oblig3.php
@@ -6,66 +6,103 @@
 		<link href="css/webcss_v2.css" type="text/css" rel="stylesheet" />
 	</head>
 	<body>
-		
-		<!-- Memos to self: (aka, den som retter behøver ikke se på dette, det er bare noe jeg eventuelt har glemt å fjerne.)
-		En div for hver oppgave, en div for hver del-oppgave.
-		-->
-		
 		<header class="box">
 			<h1 class="box">Oblig 3</h1>
             <?php include 'webutviklingMenu.php'; ?>
-            
+
             <!-- Litt generell info -->
             <div id="generelt" class="info box">
-            <h3>Informasjon fra foreleser om oppgavene</h3>
-                <p></p>
+                <h3>Informasjon fra foreleser om oppgavene</h3>
+                <p>Oppgave 1, 2 og 3 skal gjøres i de samme gruppene dere benyttet i oblig 2. Oppgave 4 og de gamle eksamensoppgavene skal gjøres individuelt.
+                <br />
+                Leveransen i oppgaven er en individuell nettside med linker til de felles gruppeoppgavene og linker til de individuelle oppgavene som er gjort. Vis tydelig i din individuelle innlevering hvem du har samarbeidet med på gruppeprosjektet.</p>
+                <p>Aktivitetsloggen på GitHub er en del av det individuelle vurderingsgrunnlaget, dette betyr at GitHub repoet må være public og må vise at alle har bidratt og at dere har hatt gode arbeidsrutiner.</p>
             </div>
 		</header>
-		
-		
-		
-		
+
 		<main class="box">
 			<!-- Øvningsoppgavene -->
 			<div class="box normal">
-				
+
 				<h2>Øvings oppgaver</h2>
-				
+
 				<!-- Oppgave 1 -->
 				<div class="box oppg" id="oppg1">
 					<h3 class="solve">Oppgave 1</h3>
 						<h4>Oppgaven</h4>
-							<p></p>
+							<p>Gjør ferdig nettstedet dere begynte på i oblig 2. Nettstedet må ikke være veldig avansert men skal vise at dere behersker HTML og CSS godt, og at dere evner å gjøre et planlagt design til et ferdig nettsted.</p>
 						<h4>Løsningen</h4>
 							<p></p>
 				</div>
-				
-			</div>
-			
-			<!-- Eksamensoppgavene -->
-			<div class="box eksamen">
-				
-				<h2>Tidligere eksamensoppgaver</h2>
-				
-				<!-- Eksamensoppgave 1 -->
-				<div class="box eksoppg" id="oppg1">
+
+				<!-- Oppgave 2 -->
+				<div class="box oppg" id="oppg2">
 					<h3 class="solve">Oppgave 2</h3>
 						<h4>Oppgaven</h4>
-							<p></p>
+							<p>Test og dokumenter at nettstedet validerer (både HTML og CSS) og at den vises som planlagt/korrekt på ulike enheter, i ulike oppløsninger og i ulike nettlesere. Det er viktig at nettstedet fungerer korrekt på mobile enheter, nettbrett, bærbare og stasjonære pcer.</p>
+                            <p>Dokumenter ved hjelp av for eksempel skjermbilder at dere har testet i forskjellige oppløsninger og i forskjellige nettlesere.</p>
+                            <p>Om dere får noen valideringsfeil som ikke kan fikses når dere tester skal dere beskrive disse og hva de betyr. Med valideringsfeil som ikke kan fikses menes browser hacks, CSS som ikke er ”ferdig” og HTML kode som ikke er ”ferdig”. Valideringsfeil som kan ordnes burde ordnes før innlevering.</p>
 						<h4>Løsningen</h4>
 							<p></p>
 				</div>
-				
+
+				<!-- Oppgave 3 -->
+				<div class="box oppg" id="oppg3">
+					<h3 class="solve">Oppgave 3</h3>
+						<h4>Oppgaven</h4>
+							<p>Lag en kort oppsummering av arbeidet og hvordan alle i gruppen har bidratt. Skriv også litt om hvordan dere har arbeidet i Git og hvordan dette har fungert for dere.</p>
+						<h4>Løsningen</h4>
+							<p></p>
+				</div>
+
+				<!-- Oppgave 4 -->
+				<div class="box oppg" id="oppg4">
+					<h3 class="solve">Oppgave 4</h3>
+						<h4>Oppgaven</h4>
+							<p>Lag en enkel nettside der du eksperimenterer med avanserte HTML og CSS funksjoner. Det som burde være med er video, CSS animasjoner, MathML, SVG og webfonter. Eksemplene skal lages selv men de trenger ikke å være veldig avanserte. Videofiler og fonter kan hentes fra nett.</p>
+						<h4>Løsningen</h4>
+							<p></p>
+				</div>
+
+			</div>
+
+			<!-- Eksamensoppgavene -->
+			<div class="box eksamen">
+
+				<h2>Tidligere eksamensoppgaver</h2>
+
+				<!-- Oppgave 5 -->
+				<div class="box eksoppg" id="oppg5">
+					<h3 class="solve">Oppgave 5</h3>
+						<h4>Oppgaven</h4>
+							<p>Det dukker stadig opp "utvidelser" til HTML-språket, og MathML er en av disse vi har sett på i kurset. Hvilke fordeler og ulemper er det ved å bruke MathML fremfor å sette inn et bilde slik det ble gjort tidligere?</p>
+						<h4>Løsningen</h4>
+							<p></p>
+				</div>
+                
+                <!-- Oppgave 6 -->
+				<div class="box eksoppg" id="oppg6">
+					<h3 class="solve">Oppgave 6</h3>
+						<h4>Oppgaven</h4>
+							<p>Hvilke fordeler/ulemper ville det hatt dersom en webside måtte være 100% korrekt/validert før den kunne vises av en nettleser? Tenk på standarder, teknologi, utviklere og brukere.</p>
+						<h4>Løsningen</h4>
+							<p></p>
+				</div>
+
 			</div>
 		</main>
-		
+
 		<aside class="quick box">
 			<ul>
 				<li><a href="#top">Top</a></li>
 				<li><a href="#oppg1">1</a></li>
 				<li><a href="#oppg2">2</a></li>
+				<li><a href="#oppg3">3</a></li>
+				<li><a href="#oppg4">4</a></li>
+				<li><a href="#oppg5">5</a></li>
+				<li><a href="#oppg6">6</a></li>
 			</ul>
 		</aside>
-		
+
 	</body>
 </html>
