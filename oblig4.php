757bc7f4c7be92b36fff8693dc90ae80e90c6a63
Irene Folkestad Sønsteby <turidfs@hiof.no>
2016-10-29 20:21:17 +0200

    Startet på opppgavene i oblig 4.

--- oblig4.php
+++ oblig4.php
@@ -55,6 +55,24 @@
                         </li>
                     </ol>
 				    <h4>Løsningen</h4>
+                    <h5>Analyse av bedriften</h5>
+                    <p></p>
+                    <dl>
+                        <dt>Målsetting</dt><dd></dd>
+                        <dt>Kundegruppen</dt><dd></dd>
+                        <dt>Produkter</dt><dd></dd>
+                        <dt>Konkurrenter</dt><dd></dd>
+                    </dl>
+                    <h5>Analyse av SEO på eksisterende nettsted (den teknisk implementasjonen)</h5>
+                    <p></p>
+                    <dl>
+                        <dt>Title tags</dt><dd></dd>
+                        <dt>nterne linker</dt><dd></dd>
+                        <dt>Innhold (der det faktisk er noe ordentlig innhold)</dt><dd></dd>
+                    </dl>
+                    <h5>SEO informasjon (hva skal gjøres og hvorfor)</h5>
+                    <p></p>
+                    <h5>Teknisk implementasjon</h5>
                     <p></p>
                     <!-- <div class="comment">
                         <h5>Feedback</h5>
@@ -71,12 +89,12 @@
                 
                 <!-- Oppgave 2 -->
 				<article class="box oppg" id="opp2">
-					<h3 class="solve">Oppgave 2</h3>
+					<h3 class="utdyp">Oppgave 2</h3>
 						<h4>Oppgaven</h4>
 							<p>Hva er mikrodata? Beskriv hva det er, hvordan det brukes og hvorfor det er nyttig. Beskriv også noen bruksområder som er spesielt egent for mikrodata.</p>
                     <p>Beskrivelsen skal være utfyllende og dekke alle viktige aspekter av Mikrodata. Om ønskelig kan du gjerne lage opp noen kode eksempler for å gjøre det enklere å beskrive vanskelig konsepter.</p>
 						<h4>Løsningen</h4>
-							<p></p>
+							<p>Oppskrifter, google søk. Merke informasjon. Mennesker, artikkler, oppskrifter, hendelser, breadcrumbs, meny, musikk.</p>
                     <!-- <div class="comment">
                         <h5>Feedback</h5>
                         <h6>Admin, positivt</h6>
@@ -103,7 +121,12 @@
 						<h4>Oppgaven</h4>
 							<p>Hva er Site Authority i forbindelse med SEO, og hvordan oppnår man dette?</p>
 						<h4>Løsningen</h4>
-							<p></p>
+							<p>Linker fra andre sider.</p>
+                    Site Authority<br />
+Hvor viktig er siden ( i henhold til søkemotorene)<br />
+Site Authority er forskjellige fra side til side på webstedet<br />
+for forskjellige søkeord<br />
+for forskjellige temaer
                     <!-- <div class="comment">
                         <h5>Feedback</h5>
                         <h6>Admin, positivt</h6>
@@ -119,12 +142,34 @@
                 
                 <!-- Oppgave 4 -->
 				<article class="box eksoppg" id="oppg4">
-					<h3 class="solve">Oppgave 4</h3>
+					<h3 class="utdyp">Oppgave 4</h3>
 						<h4>Oppgaven</h4>
 							<p>Hvilke faktorer(/elementer/parametre/informasjon) for en webside bruker en søkemotor i rangeringen? (Dette blir selvsagt ikke noen absolutt og endelig liste, men få med de viktigste)</p>
                     <p style="text-decoration: underline;"><strong>Svar som en punktliste i kortfattet form. Om det er ting du anser som spesielt viktig kan du gjerne utdype noe.</strong></p>
 						<h4>Løsningen</h4>
 							<p></p>
+                    <ul>
+                        <li>Headinger (h1 til h6)</li>
+                        <li>Strukturelle tagger</li>
+                        <li>Skalerbarhet</li>
+                        <li>Nøkkelord</li>
+                        <li>Meta taggen (til en viss grad)</li>
+                        <li>Konkurrenter, både når de gjør noe bra og når de gjør noe dårlig.</li>
+                        <li>Oppdatering av innhold</li>
+                        <li>Unikt innhold</li>
+                        <li>Skille mellom innhold, struktur, funksjonalitet osv.</li>
+                        <li>Lav bounce rate</li>
+                        <li>Alt tagger på bilder</li>
+                        <li>Validering av css og html</li>
+                        <li>Innvendig meny, gjør det enkelt å finne frem</li>
+                        <li>Linker er veldig viktig
+                            <ul>
+                                <li>Dårlige sider som linker inn gir dårligere SEO</li>
+                                <li>Bra sider som linker inn gir god SEO</li>
+                                <li>Hvor store eller små sidene som linker inn er har mye å si. Det er bedre å få en link fra Aftenposten enn fra en blog.</li>
+                            </ul>
+                        </li>
+                    </ul>
                     <!-- <div class="comment">
                         <h5>Feedback</h5>
                         <h6>Admin, positivt</h6>
@@ -140,11 +185,11 @@
                 
                 <!-- Oppgave 5 -->
 				<article class="box eksoppg" id="oppg5">
-					<h3 class="solve">Oppgave 5</h3>
+					<h3 class="utdyp">Oppgave 5</h3>
 						<h4>Oppgaven</h4>
 							<p>Hvorfor kan vi (som oftest) ikke se resultater av SEO (Search engine optimization) umiddelbart?</p>
 						<h4>Løsningen</h4>
-							<p></p>
+							<p>Google må indeksere siden på nytt.</p>
                     <!-- <div class="comment">
                         <h5>Feedback</h5>
                         <h6>Admin, positivt</h6>
--- oblig5.php
+++ oblig5.php
@@ -109,7 +109,7 @@
 				<article class="box eksoppg" id="oppg4">
 					<h3 class="solve">Oppgave 4</h3>
 				    <h4>Oppgaven</h4>
-				    <p>Norge følger etter en rekke land og innfører nå lovgivning som skal sikre universell utforming av bl.a. nettsider. (http://lovdata.no/dokument/SF/forskrift/2013-06-21-732) For oppgaven er §1,2,4,6,7 og 11 spesielt interessant. Se bort i fra det som står ang. automater.</p>
+				    <p>Norge følger etter en rekke land og innfører nå lovgivning som skal sikre universell utforming av bl.a. nettsider. (<a href="http://lovdata.no/dokument/SF/forskrift/2013-06-21-732" target="_blank">http://lovdata.no/dokument/SF/forskrift/2013-06-21-732</a>) For oppgaven er §1,2,4,6,7 og 11 spesielt interessant. Se bort i fra det som står ang. automater.</p>
                     <ol class="listLitenBokst">
                         <li>Hva tror du blir den nye lovens innvirkning på norske utviklere og brukere på kort og lang sikt. Svar fra et teknisk vinklet perspektiv.</li>
                         <li>Det Norge her gjør er lignende det flere land alt har gjort eller står på trappene for å gjøre. Hva tror du betydningen blir for standarder og verktøy/teknologi i et større perspektiv.</li>
